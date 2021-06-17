119a3bb7070c636439c1362bb9be4373acb465ac
JEFFERSON GARCIA <ibarra.jefferson1@gmail.com>
2021-06-17 12:31:14 -0500

    Primer envio

---
+++ cmake-build-debug/CMakeFiles/recargomatricula2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/recargomatricula2.dir/main.cpp.obj"
+  "librecargomatricula2.dll.a"
+  "recargomatricula2.exe"
+  "recargomatricula2.exe.manifest"
+  "recargomatricula2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/recargomatricula2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/recargomatricula2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for recargomatricula2.
+# This may be replaced when dependencies are built.
